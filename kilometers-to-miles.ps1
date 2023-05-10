e9c7d37099e13aa3fdce78403d82a86ea48dc936
Mustafa-Almohsen <133123192+Mustafa-Almohsen@users.noreply.github.com>
2023-05-10 19:54:24 +0300

    Create kilometers-to-miles.ps1

---
+++ kilometers-to-miles.ps1
@@ -0,0 +1,25 @@
+$kilometers = read-host "Enter kilometers to be converted to miles"
+$Miles = $kilometers / 1 * 0.62
+write-output "$kilometers kilometers is $Miles miles!"
+read-host "Press any key to exit"
+
+
+
+
+Go to Go to https://bit.ly/3DZDxvc and use which mathematical formula to check the kilometers
+converted to miles.
+
+Here's the breakdown of the script:
+
+# Prompt the user to enter kilometers to be converted to miles
+$kilometers = read-host "Enter kilometers to be converted to miles"
+
+# Perform the conversion calculation: kilometers to miles
+$Miles = $kilometers / 1 * 0.62
+
+# Output the result: kilometers to miles
+write-output "$kilometers kilometers is $Miles miles!"
+
+# Prompt the user to press any key before exiting
+read-host "Press any key to exit"
+
